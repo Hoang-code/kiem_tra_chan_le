6a726e0fa26e30bc29e74f118988dadeafe4e321
Hoang-code <71956596+Hoang-code@users.noreply.github.com>
2020-10-19 21:42:08 +0700

    addbien

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function kiemtrachanle(){
+    let a = number(document.getElementById("number").value)
+    console.log (nhapso)
+    let b = a % 2
+    console.log ()
+    if (b == "0"){
+        document.getElementById("hienketqua").innerText = "số vừa nhập là số chẵn"
+        console.log(hiensochan)
+    }
 
 }
