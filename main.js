3b31d4dfebbce85415f044fa3010a403b9e193ca
Hoang-code <71956596+Hoang-code@users.noreply.github.com>
2020-10-20 20:32:36 +0700

    hoanthien

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ function kiemtrachanle(){
     let a = Number(document.getElementById("dienso").value)
     let b = a % 2
         if (b == 0){
-            document.getElementById("hienketqua").innerText = a + " " + "là số chẵn"
+            document.write( a + " " + "là số chẵn")
+            //document.getElementById("hienketqua").innerText = a + " " + "là số chẵn"
        }else{
-            document.getElementById("hienketqua"),innerText = a + "là số lẻ"
+            document.write(a + " " + "là số lẻ")
+            //document.getElementById("hienketqua"),innerText = a + " " + "là số lẻ"
         }
 
 }
